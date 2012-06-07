f3547ef2bdc3f595459bde21f3ab58ff61f3c8f3
unknown <jayakumar.m.h@gmail.com>
2012-06-07 21:55:40 +0100

    initial code

---
+++ lovemyfilm/LoveMyFilm/LoveMyFilm.DataLayer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoveMyFilm.DataLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ lovemyfilm/LoveMyFilm/LoveMyFilm.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoveMyFilm.Web.MvcApplication" Language="C#" %>
